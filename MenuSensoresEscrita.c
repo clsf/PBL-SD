4022154ecd246e3c4745c7ab0328d807811f2731
Cláudia <96408566+clsf@users.noreply.github.com>
2023-09-10 10:18:14 -0300

    Add files via upload

--- MenuSensoresEscrita.c
+++ MenuSensoresEscrita.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
+//#include <unistd.h>
+//#include <fcntl.h>
+//#include <termios.h>
 
 void printMenuSensor(){
     printf("\n==== SENSORES ====\n");
@@ -26,10 +26,9 @@ int main (){
     int sensor;
     int comando;
     int continuar = 1;
-    char byte1Sensor[255];
-    char byte2Comando[255];
+    char bytes[2];
     struct termios options;
-    int fd, len1,len2;
+    int fd, len;
 
     // Configuracoes
     fd = open("/dev/ttyS0", O_WRONLY);
@@ -43,7 +42,7 @@ int main (){
       CS8 - usa 8 bits
     */
    options.c_cflag = B9600 | CS8 | CLOCAL| CREAD;
-   //cfsetspeed(&options, B9600);
+   cfsetspeed(&options, B9600);
 
    //input flags
    options.c_iflag  = IGNPAR;
@@ -62,23 +61,14 @@ int main (){
                 scanf("%d", &comando);
 
                 if(comando >= 1 && comando <= 7){
-                    int tamanhoMaximo1 = sizeof(byte1Sensor);
-                    int tamanhoMaximo2 = sizeof(byte2Comando);
-                    snprintf(byte1Sensor, tamanhoMaximo1, "%d", sensor);
-                    snprintf(byte2Comando, tamanhoMaximo2, "%d", comando);
-                   // printf("sensor: %s\n", byte1Sensor);
-                   // printf("comando: %s\n", byte2Comando);
+                    sprintf(bytes, "%d%d", sensor,comando);
+                    printf("Voce vai escrever os caracteres %s\n", bytes);
 
-                    len1 = strlen(byte1Sensor);
-                    printf("Tamanho byte 1: %d\n", len1);
-                    len1 = write(fd, byte1Sensor, len1);
-                    printf("Primeiro byte enviado:  %d \n", len1);
-                    sleep(0.5);
-                    len2 = strlen(byte2Comando);
-                    printf("Tamanho byte 2: %d\n", len2);
-                    len2 = write(fd, byte2Comando, len2);
-                   printf("Segundo byte enviado:  %d \n", len2);
-                    sleep(0.5);
+
+                    len = strlen(bytes);
+                    len = write(fd, bytes, len);
+                    printf("Bytes enviados:  %d \n", len1);
+                    sleep(1);
 
                 }else{
                     printf("\nOpcao de comando invalida!\n");
